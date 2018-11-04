b480c95b4e5d3991d8ad3036aa0d0ca8f251edd1
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-04 21:36:04 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+class Application
+
+  @@items = []
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/item/)
+      item_name = req.path.split('/').last
+      item = @@items.detect { |item| item.name == item_name }
+      if item
+        resp.write item.price
+      else
+        resp.status = 400
+        resp.write "Item not found"
+      end
+    else
+      resp.status = 404
+      resp.write "Route not found"
+    end
+
+    resp.finish
+  end
+end
