de80f6053004a0ecb68693a13a3c1bb135379d95
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-04 21:24:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,13 +1,14 @@
 class Application
 
-  @@item = []
-  
+  @@items = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/item/)
-
+      binding.pry
+      if @@items.include?(
     else
       resp.status = 404
       resp.write "Route not found"
