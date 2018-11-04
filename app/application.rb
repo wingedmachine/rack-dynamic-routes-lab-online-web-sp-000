790509d7d7a7a227e27f7fe33fa0154841d580e1
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-04 21:31:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
     if req.path.match(/item/)
       item_name = req.path.split('/').last
       item = @@items.detect { |item| item.name = item_name}
-      pry
+      binding.pry
       if item
         resp.write item.price
       else
